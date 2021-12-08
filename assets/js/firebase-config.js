8ba7313ce335dc671e0813f04dba72fc73b0afe2
filip <ja.lip132@gmail.com>
2021-12-08 07:49:08 +0100

    feat: firebase crud and bootstrap boilerplate

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,22 +1,23 @@
-
-var app_fireBase = {};
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 //inicijalizacija baze podataka(firebase-a)
-var firebaseConfig = {
-    apiKey: "AIzaSyB5zEwnyjckAHvhImzBNFKJDdQG5Y0JdlM",
-    authDomain: "zavrsniradoviowp.firebaseapp.com",
-    databaseURL: "https://zavrsniradoviowp.firebaseio.com",
-    projectId: "zavrsniradoviowp",
-    storageBucket: "zavrsniradoviowp.appspot.com",
-    messagingSenderId: "267891726660",
-    appId: "1:267891726660:web:09b945ebbb6a7725c51f11",
-    measurementId: "G-E4VXK2QXFG"
+const firebaseConfig = {
+    apiKey: "AIzaSyByKK5JwSGDlwlgVwJfqy6qffZ_lzE7MEA",
+    authDomain: "task-management-452a7.firebaseapp.com",
+    databaseURL: "https://task-management-452a7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "task-management-452a7",
+    storageBucket: "task-management-452a7.appspot.com",
+    messagingSenderId: "886659271691",
+    appId: "1:886659271691:web:3d6eed9ee2aa2944fe0b0e",
+    measurementId: "${config.measurementId}"
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 //kreiranje objekta firebase baze
-app_fireBase = firebase;
+app_fireBase = app;
 var oDb = firebase.database();
 
 var oDbUsers = oDb.ref('users');
